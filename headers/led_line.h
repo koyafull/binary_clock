ed9a6671245ab7eff8b2d163f07bb6fb266992b8
François Uhl <francois.uhl@metronlab.com>
2017-10-09 23:09:12 +0200

    fixed: private attribute

--- headers/led_line.h
+++ headers/led_line.h
@@ -1,12 +1,12 @@
 #include <iostream>
-#include <ctime>
 #include <stdint.h>
 
 class led_line
 {
-	public:
+	private:
 	int16_t digit;
 
+	public:
 	led_line(int);
 	bool * int_to_binary();
 	void print_binary(const bool *);
