f714a68a086ed276f2d26b3408074cf841d60b90
François Uhl <francois.uhl@metronlab.com>
2017-11-18 00:05:04 +0100

    added: while true now on

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <ctime>
 #include <iostream>
-#include <stdint.h>
+#include <unistd.h>	// for usleep()
 #include <stdlib.h>
 #include <signal.h>
 
@@ -23,31 +23,25 @@ int main() {
 
 	signal(SIGINT, sigint_handler);
 
-	time_t rawtime;
-   	struct tm * timeinfo;
-	char buffer[10];
-
-	time(&rawtime);
-	timeinfo = localtime(&rawtime);
-
-	strftime(buffer, 10, "%H:%M:%S", timeinfo);
-	cout << buffer << endl;
-	for(int i = 0 ; i < 10 ; i++)
+	while(1)
 	{
-		cout << i << "-" << ctoi(buffer[i]) << endl;
+		time_t rawtime;
+   		struct tm * timeinfo;
+		char buffer[10];
+
+		time(&rawtime);
+		timeinfo = localtime(&rawtime);
+
+		strftime(buffer, 10, "%H:%M:%S", timeinfo);
+		cout << buffer << endl;
+//		for(int i = 0 ; i < 10 ; i++)
+//		{
+//			cout << i << "-" << ctoi(buffer[i]) << endl;
+//		}
+//		cout << endl;
+
+		led_panel current_panel = led_panel(ctoi(buffer[0]), ctoi(buffer[1]), ctoi(buffer[3]), ctoi(buffer[4]), ctoi(buffer[6]), ctoi(buffer[7]));
+		current_panel.print_binary_clock();
+		usleep(1000000);
 	}
-	cout << endl;
-
-//	int test(5);
-//	led_line ll = led_line(test);
-//	ll.print_binary();
-
-	led_panel current_panel = led_panel(ctoi(buffer[0]), ctoi(buffer[1]), ctoi(buffer[3]), ctoi(buffer[4]), ctoi(buffer[6]), ctoi(buffer[7]));
-//	cout << "print_binary" << endl;
-//	current_panel.print_binary();
-	cout << endl << endl << "print_binary_clock" << endl;
-	current_panel.print_binary_clock();
-
-	char a;
-	cin >> a;
 }
