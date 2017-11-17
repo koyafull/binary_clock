f1d2d0eaf5e52865280df37bffb13f578659d627
François Uhl <francois.uhl@metronlab.com>
2017-11-17 22:51:55 +0100

    added: sig handler, time management: parsing and linking with previous work

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,53 @@
 #include <ctime>
 #include <iostream>
 #include <stdint.h>
+#include <stdlib.h>
+#include <signal.h>
 
-//#include "../headers/led_line.h"
 #include "../headers/led_panel.h"
 
 using namespace std;
 
+int ctoi(char c)
+{
+	return c - '0';
+}
+
+void sigint_handler(int signum)
+{
+	cout << endl << endl << "          ==================== SIGINT CAUGHT ====================" << endl << "                                   exiting" << endl << endl;
+	exit(signum);
+}
+
 int main() {
-	time_t t = time(0);   // get time now
-   	tm * now = localtime( & t );
-//	cout << now->tm_year << endl;
+
+	signal(SIGINT, sigint_handler);
+
+	time_t rawtime;
+   	struct tm * timeinfo;
+	char buffer[10];
+
+	time(&rawtime);
+	timeinfo = localtime(&rawtime);
+
+	strftime(buffer, 10, "%H:%M:%S", timeinfo);
+	cout << buffer << endl;
+	for(int i = 0 ; i < 10 ; i++)
+	{
+		cout << i << "-" << ctoi(buffer[i]) << endl;
+	}
+	cout << endl;
+
 //	int test(5);
 //	led_line ll = led_line(test);
 //	ll.print_binary();
 
-	led_panel current_panel = led_panel(1, 2, 5, 1, 7, 4);
-	cout << "print_binary" << endl;
-	current_panel.print_binary();
+	led_panel current_panel = led_panel(ctoi(buffer[0]), ctoi(buffer[1]), ctoi(buffer[3]), ctoi(buffer[4]), ctoi(buffer[6]), ctoi(buffer[7]));
+//	cout << "print_binary" << endl;
+//	current_panel.print_binary();
 	cout << endl << endl << "print_binary_clock" << endl;
 	current_panel.print_binary_clock();
+
+	char a;
+	cin >> a;
 }
