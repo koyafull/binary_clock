927b716382bd5bec7a674e9391eed93f97e2b0c9
François Uhl <francois.uhl@metronlab.com>
2019-01-29 23:28:24 +0100

    added: led_line comments

--- headers/led_line.h
+++ headers/led_line.h
@@ -1,3 +1,16 @@
+//////////////////////////////////////////////////////////////////////////
+//									//
+//	led_line obecjt represents one digit among the 6 that are 	//
+//	needed to form a standard hour format: HH:MM:SS. This digit is 	//
+//	in its bit shape and is align vertically on the final binary 	//
+//	clock. The number of maximum bits that you need to represent a  //
+// 	digit is variable: for the tens of hours, the max is 2, so 2	//
+//	leds are enough, for unit of seconds, the max is 9, so 4 leds 	//
+//	are needed. Whatever, a led_line is considered as 8 bits long.	//
+//									//
+//////////////////////////////////////////////////////////////////////////
+
+
 #include <iostream>
 #include <stdint.h>
 #include <vector>
@@ -9,7 +22,12 @@ class led_line
 	int size;
 
 	public:
+
+	/// constructor
 	led_line(int);
+
+	/// print to stdout a formal representation of the line. 
+	/// Ex: 00000101
 	const void print_binary();
 
 	friend class led_panel;
