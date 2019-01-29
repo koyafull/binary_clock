d038588d87c6c793d4d5fefba7a8ac9fd1e4a2d3
François Uhl <francois.uhl@metronlab.com>
2019-01-29 23:05:31 +0100

    added: comments on main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
+//////////////////////////////////////////////////////////////////////////
+//									//
+//	main method that basically:					//
+//		* instanciates the program environment, such as signal	//
+//		interpretation, environment variables and GPIO settings	//
+//		* runs a infinite loop that transform uptime syscall to	//
+//		shinning leds						//
+//									//
+//	Handles the SINGINT (15) signal to exit gracefully.		//
+//									//
+//////////////////////////////////////////////////////////////////////////
+
 #include <ctime>
 #include <iostream>
-#include <unistd.h>	// for usleep()
+#include <unistd.h>	// usleep()
 #include <stdlib.h>
 #include <signal.h>
-#include <utility>	// pair
+#include <utility>	// pair<T, T>
 #include <map>
 
 #include "../headers/led_panel.h"
@@ -11,20 +23,25 @@
 using namespace std;
 
 int ctoi(char c)
+/// converts a char <c> to an int
 {
 	return c - '0';		// ASCII trick to turn a single char digit to an int digit
 }
 
 void sigint_handler(int signum)
+/// signal handler: defines the beavior of signal <sigmun> catching
+// param:
+//  signum: number of signal to have behavior redefined
 {
 	cout << endl << endl << "          ==================== SIGINT CAUGHT ====================" << endl << "                                   exiting" << endl << endl;
-	throw 20;		// to call destructor and close properly, we need to exit from main(), so we forward
+	throw 20;		// to call led_panel destructor and close properly, we need to exit from main(), so we forward
 }
 
-int main() {
+int main()
+{
 	try
 	{
-
+		/// environment variable creation
 		map<pair<int, int>, int> gpio_panel;
 		gpio_panel[make_pair(6,0)] = 14;
 		gpio_panel[make_pair(7,0)] = 4;
@@ -47,36 +64,39 @@ int main() {
 		gpio_panel[make_pair(6,5)] = 6;
 		gpio_panel[make_pair(7,5)] = 5;
 
+		/// signal handling: redirects signal SINGINT (15) to sigint_handler. See signal for more information about signal and signal handling.
 		signal(SIGINT, sigint_handler);
 
+		/// time management
+			/// creates time structure
 		time_t rawtime;
 		struct tm * timeinfo;
 		char buffer[10];
-		time(&rawtime);
-		timeinfo= localtime(&rawtime);
+
+		/// loop initialization
+		time(&rawtime);			// gets current time
+		timeinfo = localtime(&rawtime);	// converts it to friendly object
 
 		led_panel current_panel = led_panel(gpio_panel, ctoi(buffer[0]), ctoi(buffer[1]), ctoi(buffer[3]), ctoi(buffer[4]), ctoi(buffer[6]), ctoi(buffer[7]));
 
 		while(1)
 		{
-			time(&rawtime);
-			timeinfo = localtime(&rawtime);
+			/// infinite loop
+				/// time management
+			time(&rawtime);					// gets current time
+			timeinfo = localtime(&rawtime);			// converts it to friendly object
 
-			strftime(buffer, 10, "%H:%M:%S", timeinfo);
+			strftime(buffer, 10, "%H:%M:%S", timeinfo);	// fills buffer with specific format
 			cout << buffer << endl;
-	//		for(int i = 0 ; i < 10 ; i++)
-	//		{
-	//			cout << i << "-" << ctoi(buffer[i]) << endl;
-	//		}
-	//		cout << endl;
 
+				/// led_panel management
 			current_panel.set_led_panel(ctoi(buffer[0]), ctoi(buffer[1]), ctoi(buffer[3]), ctoi(buffer[4]), ctoi(buffer[6]), ctoi(buffer[7]));
-//			current_panel.set_led_panel(0,0,0,4,0,0);
 			current_panel.print_binary_clock();
 			current_panel.print_gpio();
 			usleep(1000000);
 		}
 	}
+	/// sigint graceful exiting
 	catch(int e)
 	{
 		if(e == 20)
